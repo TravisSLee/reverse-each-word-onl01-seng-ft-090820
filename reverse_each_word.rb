72e27a1834f8f2fa8baabe8ba244e03ff2daaac1
Travis Lee <travisslee223@gmail.com>
2020-09-11 17:57:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence)
   words = sentence.split(" ")
+  reversed_words = []
   reversed_words = words.each do |word|
-    
+    reversed_words
   end
   reversed_words.join(" ")
 end
